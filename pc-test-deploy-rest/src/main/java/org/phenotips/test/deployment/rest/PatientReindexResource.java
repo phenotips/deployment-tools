45f44e08fae888478e51608b11e9cc1e482f2ba1
veronikaslc <veronika.koltunov@gmai.com>
2018-10-26 23:02:21 -0400

    [rest] Added reindex REST service

---
+++ pc-test-deploy-rest/src/main/java/org/phenotips/test/deployment/rest/PatientReindexResource.java
@@ -0,0 +1,50 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/
+ */
+package org.phenotips.test.deployment.rest;
+
+import org.phenotips.data.rest.PatientResource;
+import org.phenotips.rest.ParentResource;
+
+import org.xwiki.stability.Unstable;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ * Reindex patients. Needed because Solr does not triggered on XAR import and imported patients are not indexed.
+ *
+ * @version $Id: 4aedac8a38e3db2e746c5925cc00c29d6730e28e $
+ * @since 1.1
+ */
+@Unstable("New API introduced in 1.2")
+@Path("/patients/reindex")
+@ParentResource(PatientResource.class)
+public interface PatientReindexResource
+{
+    /**
+     * Reindex patients.
+     *
+     * @return a response
+     */
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    Response reindexPatients();
+}
---
+++ pc-test-deploy-rest/src/main/java/org/phenotips/test/deployment/rest/internal/DefaultPatientReindexResource.java
@@ -0,0 +1,57 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/
+ */
+package org.phenotips.test.deployment.rest.internal;
+
+import org.phenotips.data.indexing.PatientIndexer;
+import org.phenotips.test.deployment.rest.PatientReindexResource;
+
+import org.xwiki.component.annotation.Component;
+import org.xwiki.rest.XWikiResource;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.inject.Singleton;
+import javax.ws.rs.core.Response;
+
+
+/**
+ * Default implementation of the {@link PatientReindexResource}.
+ *
+ * @version $Id: 8afa6c9c7daa868f7c3b1fe50289dfa17d3b510b $
+ * @since 1.1
+ */
+@Component
+@Named("org.phenotips.test.deployment.rest.internal.DefaultPatientReindexResource")
+@Singleton
+public class DefaultPatientReindexResource extends XWikiResource implements PatientReindexResource
+{
+    @Inject
+    private PatientIndexer indexer;
+
+    @Override
+    public Response reindexPatients()
+    {
+        try {
+            this.indexer.reindex();
+            return Response.status(Response.Status.OK).build();
+        } catch (final IndexOutOfBoundsException e) {
+            this.slf4Jlogger.error("Error while reindexing patients: {}", e);
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        }
+    }
+}

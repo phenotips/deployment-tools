9393f8dd018f92bdaa0c25738cde64bb19bfb687
veronikaslc <veronika.koltunov@gmail.com>
2018-10-31 17:05:59 -0400

    [script] Added security group to the parameters for server creation Minor code refactoring

--- scripts/openstack_vm_deploy.py
+++ scripts/openstack_vm_deploy.py
@@ -22,6 +22,7 @@ KEYPAIR_NAME = 'PCMain'
 NETWORK_NAME = "TestPC"
 KID_NETWORK_NAME = "Kidnet External"
 EXCLUDE_SERVER_PREFIX = "PC_deployment"
+SECURITY_GROUP_NAME = "ingress_cidr_local_tcp_8080"
 #####################################################
 
 # script parameters
@@ -44,24 +45,7 @@ def script(settings):
     logging.info("Connected to OpenStack")
 
     if settings.action == 'list':
-        # openstack server list
-        servers_list = conn.compute.servers()
-        logging.info("List: {0}".format(str(servers_list)))
-        data = []
-        for server in servers_list:
-            ipf = ''
-            if NETWORK_NAME not in server.addresses.keys():
-                # exclude servers not inthe PC deployment network
-                continue
-            if server.name.startswith(EXCLUDE_SERVER_PREFIX):
-                # exclude the frontend itself, and any other development servers
-                continue
-            logging.info("Listing server : {0}".format(server.name))
-            for address in server.addresses[NETWORK_NAME]:
-                if address['OS-EXT-IPS:type'] == 'floating':
-                    ipf = address['addr']
-            data.append({'id' : server.id, 'name' : server.name, 'ip' : ipf, 'created' : server.created_at, 'status' : server.vm_state, 'metadata' : server.metadata})
-        print(data, file=open(SERVER_LIST_FILE_NAME, "w"))
+        list_servers(conn)
         sys.exit(0)
 
     # find if there already exists a VM with the given name
@@ -69,16 +53,30 @@ def script(settings):
 
     # if a VM with the same build name already exists - delete it
     if server:
-        logging.info("Server for build %s exists" % settings.build_name)
-        logging.info(server)
-        logging.info("deleting server..........")
+        logging.info("Server for build %s exists, deleting server.........." % settings.build_name)
         conn.compute.delete_server(server, ignore_missing=True, force=True)
         conn.compute.wait_for_delete(server)
-        logging.info("server %s deleted" % settings.build_name)
+        logging.info("Server %s deleted" % settings.build_name)
 
     if settings.action == 'delete':
         sys.exit(0)
 
+    server = create_server(conn, settings)
+    add_floatingip(conn, server)
+
+def add_floatingip(conn, server):
+    # openstack server add floating ip [build_name] [ip]
+    logging.info("Assigning floating IPs..........")
+    fip = get_floating_ip(conn)
+    #server.add_floating_ip(address=fip.floating_ip_address)
+    retcode = subprocess.call(['openstack', 'server', 'add', 'floating', 'ip', server.name, fip.floating_ip_address])
+    if retcode != 0:
+        logging.error('Error: assiging floating_ip_address {0} failed'.format(fip.floating_ip_address))
+        sys.exit(-4)
+    else:
+        logging.info("-- FLOATING IP ASSOCIATED: {0}".format(fip))
+
+def create_server(conn, settings):
     # Get server metadata
     # server_metadata = conn.compute.get_server_metadata(server).metadata
 
@@ -86,6 +84,7 @@ def script(settings):
     flavor = conn.compute.find_flavor(FLAVOUR)
     network = conn.network.find_network(NETWORK_NAME)
     keypair = conn.compute.find_keypair(KEYPAIR_NAME)
+    sgroup = conn.network.find_security_group(SECURITY_GROUP_NAME)
 
     metadatau = {}
     metadatau['pn'] = settings.pn_branch_name
@@ -98,33 +97,43 @@ def script(settings):
     try:
         server = conn.compute.create_server(
             name=settings.build_name, image_id=image.id, flavor_id=flavor.id,
-            networks=[{"uuid": network.id}], key_name=keypair.name, metadata=metadatau)
+            networks=[{"uuid": network.id}], security_groups=[{"name": sgroup.name}],
+            key_name=keypair.name, metadata=metadatau)
 
         # Wait for a server to be in a status='ACTIVE', failures=None, interval=2, wait=120
         # interval – Number of seconds to wait before to consecutive checks. Default to 2.
         # wait – Maximum number of seconds to wait before the change. Default to 120.
         server = conn.compute.wait_for_server(server, interval=30, wait=600)
+        return server
     except:
         logging.info("-- FAILED TO START A VM (timeout?)")
         server = conn.compute.find_server(settings.build_name)
         if server:
             logging.info("-- STATUS: {0}".format(server.status))
+            return server
         else:
             logging.info("-- VM with name {0} not found".format(settings.build_name))
         sys.exit(-3)
 
-    #print("ssh -i {key} root@{ip}".format(key=PRIVATE_KEYPAIR_FILE, ip=server.access_ipv4))
-
-    # openstack server add floating ip [build_name] [ip]
-    logging.info("Assigning floating IPs..........")
-    fip = get_floating_ip(conn)
-    #server.add_floating_ip(address=fip.floating_ip_address)
-    retcode = subprocess.call(['openstack', 'server', 'add', 'floating', 'ip', settings.build_name, fip.floating_ip_address])
-    if retcode != 0:
-        logging.error('Error: assiging floating_ip_address {0} failed'.format(fip.floating_ip_address))
-        sys.exit(-4)
-    else:
-        logging.info("-- FLOATING IP ASSOCIATED: {0}".format(fip))
+def list_servers(conn):
+    # openstack server list
+    servers_list = conn.compute.servers()
+    logging.info("List: {0}".format(str(servers_list)))
+    data = []
+    for server in servers_list:
+        ipf = ''
+        if NETWORK_NAME not in server.addresses.keys():
+            # exclude servers not inthe PC deployment network
+            continue
+        if server.name.startswith(EXCLUDE_SERVER_PREFIX):
+            # exclude the frontend itself, and any other development servers
+            continue
+        logging.info("Listing server : {0}".format(server.name))
+        for address in server.addresses[NETWORK_NAME]:
+            if address['OS-EXT-IPS:type'] == 'floating':
+                ipf = address['addr']
+        data.append({'id' : server.id, 'name' : server.name, 'ip' : ipf, 'created' : server.created_at, 'status' : server.vm_state, 'metadata' : server.metadata})
+    print(data, file=open(SERVER_LIST_FILE_NAME, "w"))
 
 # Retrieves an un-associated floating ip if available (once that dont have Fixed IP Address), or allocates 1 from pool
 def get_floating_ip(conn):
